ad7d819aa74dd2fb50e8e5b1a3aea20db95ec76b
Kayo.io <kayorenatocontato@gmail.com>
2022-07-02 19:15:07 -0300

    BEGIN: Creation of RFM Script for customer segmentation

---
+++ Segmentacao-Cliente-RFM.R
@@ -0,0 +1,12 @@
+
+# Problema de Negocio - Realizar segmentacao de clientes com base em analise RFM (Recencia, Frequencia e Valor Monetario)
+
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(caret)
+library(plotly)
+library(readxl)
+library(rfm)
+library(stats)
+library(factoextra)
